031ed288eaba0f64c5378b08d7ac310a3f71e437
Alcaide F. Inocente <ialcaidef@hotmail.com>
2020-12-06 18:51:40 +0100

    Add project files.

---
+++ EntityFrameworkExample/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkExample.Data;
+using Microsoft.EntityFrameworkCore;
+using EntityFrameworkExample.Data;
+
+namespace EntityFrameworkExample
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<PersonContext>(options =>
+                options.UseSqlite("Data Source=person.db"));
+        }
+
+        //public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, PersonContext personContext)
+        {
+            personContext.Database.EnsureDeleted();
+            personContext.Database.EnsureCreated();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "defaultRoute",
+                    template: "{controller=Person}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
